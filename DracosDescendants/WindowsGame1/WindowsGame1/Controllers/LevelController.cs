781ee5ae3571fcb9916316229f7a45d827bd748f
Michael Jalkio <mjalkio@gmail.com>
2014-03-16 20:25:42 -0400

    LevelController Skeleton
    Created the basic interface for the LevelController.

--- DracosDescendants/WindowsGame1/WindowsGame1/Controllers/LevelController.cs
+++ DracosDescendants/WindowsGame1/WindowsGame1/Controllers/LevelController.cs
@@ -1,11 +1,45 @@
-﻿using System;
+﻿#region Using Statements
+using DracosD.Models;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
+using Microsoft.Xna.Framework;
+using Microsoft.Xna.Framework.Graphics;
+#endregion
 
 namespace DracosD.Controllers
 {
     class LevelController
     {
+        #region Fields
+        private List<Dragon> racerList;
+        private List<Gate> gateList;
+        private List<PlanetaryObject> planetList;
+        private int levelHeight;
+        private int levelWidth;
+        private Texture2D background;
+        #endregion
+
+        #region Properties (READ-ONLY)
+        public List<Dragon> Racers
+        {
+            get { return racerList; }
+        }
+
+        public List<PlanetaryObject> Planets
+        {
+            get { return planetList; }
+        }
+
+        public List<Gate> Gates
+        {
+            get { return gateList; }
+        }
+
+        public Vector4 Dimensions
+        {
+            get { return new Vector4(0, 0, levelWidth, levelHeight); }
+        }
+        #endregion
+
+
     }
 }
